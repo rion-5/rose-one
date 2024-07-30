abcda526481ef27f0c9032d97507124b170a68bf
rion-5 <sklee@hanyang.ac.kr>
2024-07-30 15:39:31 +0900

    draw2 stock line-chart

--- src/routes/stock/+page.svelte
+++ src/routes/stock/+page.svelte
@@ -11,10 +11,6 @@
 		tradeData: TradeData[];
 	};
 
-	// onMount(()=>{
-	// 	console.log(data.tradeData);
-	// });
-
 	onMount(() => {
 		if (data.tradeData && data.tradeData.length > 0) {
 			// 데이터의 날짜를 Date 객체로 변환
@@ -22,88 +18,84 @@
 				...d,
 				trading_date: new Date(d.trading_date)
 			}));
-
-			// 라인 차트를 그리는 함수
-			const drawLineChart = (data: TradeData[]) => {
-				const margin = { top: 40, right: 30, bottom: 25, left: 40 };
-				const width = 800;
-				const height = 400;
-				const innerWidth = width - margin.left - margin.right;
-				const innerHeight = height - margin.top - margin.bottom;
-				const color = '#1f77b4';
-
-				const svg = d3
-					.select('#line-chart')
-					.append('svg')
-					.attr('viewBox', `0, 0, ${width}, ${height}`)
-					.attr('width', '100%')
-					.attr('height', '100%');
-
-				const innerChart = svg
-					.append('g')
-					.attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-				// xScale 정의
-				const xScale = d3
-					.scaleTime()
-					.domain(d3.extent(data, (d) => d.trading_date) as [Date, Date])
-					.range([0, innerWidth]);
-
-				// yScale 정의
-				const yScale = d3
-					.scaleLinear()
-					.domain(d3.extent(data, (d) => d.close) as [number, number])
-					.range([innerHeight, 0]);
-
-				// x축 생성
-				const bottomAxis = d3
-					.axisBottom(xScale)
-					.tickFormat((d) => d3.timeFormat('%m/%d')(d as Date));
-
-				innerChart
-					.append('g')
-					.attr('class', 'axis-x')
-					.attr('transform', `translate(0, ${innerHeight})`)
-					.call(bottomAxis);
-
-				// y축 생성
-				innerChart.append('g').call(d3.axisLeft(yScale));
-
-				// 라인 생성
-				const line = d3
-					.line<TradeData>()
-					.x((d) => xScale(d.trading_date))
-					.y((d) => yScale(d.close));
-
-				innerChart
-					.append('path')
-					.datum(data)
-					.attr('fill', 'none')
-					.attr('stroke', color)
-					.attr('stroke-width', 2)
-					.attr('d', line);
-
-				innerChart
-					.selectAll('circle')
-					.data(data)
-					.join('circle')
-					.attr('r', 2)
-					.attr('cx', (d) => xScale(d.trading_date))
-					.attr('cy', (d) => yScale(d.close))
-					.attr('fill', color);
-
-				innerChart
-					.append('text')
-					.text('Stock Price')
-					.attr('x', xScale(data[data.length - 1].trading_date) + 10)
-					.attr('y', yScale(data[data.length - 1].close))
-					.attr('dominant-baseline', 'middle')
-					.attr('fill', color);
-			};
-
 			drawLineChart(parsedData);
 		}
 	});
+	// 라인 차트를 그리는 함수
+	const drawLineChart = (data: TradeData[]) => {
+		const margin = { top: 40, right: 30, bottom: 25, left: 40 };
+		const width = 800;
+		const height = 400;
+		const innerWidth = width - margin.left - margin.right;
+		const innerHeight = height - margin.top - margin.bottom;
+		const color = '#1f77b4';
+
+		const svg = d3
+			.select('#line-chart')
+			.append('svg')
+			.attr('viewBox', `0, 0, ${width}, ${height}`)
+			.attr('width', '100%')
+			.attr('height', '100%');
+
+		const innerChart = svg
+			.append('g')
+			.attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+		// xScale 정의
+		const xScale = d3
+			.scaleTime()
+			.domain(d3.extent(data, (d) => d.trading_date) as [Date, Date])
+			.range([0, innerWidth]);
+
+		// yScale 정의
+		const yScale = d3
+			.scaleLinear()
+			.domain(d3.extent(data, (d) => d.close) as [number, number])
+			.range([innerHeight, 0]);
+
+		// x축 생성
+		const bottomAxis = d3.axisBottom(xScale).tickFormat((d) => d3.timeFormat('%m/%d')(d as Date));
+
+		innerChart
+			.append('g')
+			.attr('class', 'axis-x')
+			.attr('transform', `translate(0, ${innerHeight})`)
+			.call(bottomAxis);
+
+		// y축 생성
+		innerChart.append('g').call(d3.axisLeft(yScale));
+
+		// 라인 생성
+		const line = d3
+			.line<TradeData>()
+			.x((d) => xScale(d.trading_date))
+			.y((d) => yScale(d.close));
+
+		innerChart
+			.append('path')
+			.datum(data)
+			.attr('fill', 'none')
+			.attr('stroke', color)
+			.attr('stroke-width', 2)
+			.attr('d', line);
+
+		innerChart
+			.selectAll('circle')
+			.data(data)
+			.join('circle')
+			.attr('r', 2)
+			.attr('cx', (d) => xScale(d.trading_date))
+			.attr('cy', (d) => yScale(d.close))
+			.attr('fill', color);
+
+		innerChart
+			.append('text')
+			.text('Stock Price')
+			.attr('x', xScale(data[data.length - 1].trading_date) + 10)
+			.attr('y', yScale(data[data.length - 1].close))
+			.attr('dominant-baseline', 'middle')
+			.attr('fill', color);
+	};
 </script>
 
 <div id="line-chart" style="width: 100%; height: 400px;"></div>
