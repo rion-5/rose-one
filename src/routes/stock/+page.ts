3f568d36576d7b9b211b8506b9c2b86a2ca56377
rion-5 <sklee@hanyang.ac.kr>
2024-08-01 16:03:34 +0900

    edit api url

--- src/routes/stock/+page.ts
+++ src/routes/stock/+page.ts
@@ -5,8 +5,11 @@ interface TradeData {
     close: string;
 }
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+
 export const load: PageLoad = async ({ fetch }) => {
-    const response = await fetch('${apiBaseUrl}/tradeDataForChart?symbol=TSLA&start_date=2024-01-01&end_date=2024-07-30');
+    const response = await fetch(`${apiBaseUrl}/tradeDataForChart?symbol=TSLA&start_date=2024-01-01&end_date=2024-07-30`);
 
     if (!response.ok) {
         throw new Error('Failed to fetch data');
