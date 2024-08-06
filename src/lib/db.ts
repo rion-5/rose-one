733091bc6cc03ced11861553cd6ef43af007c7e7
rion-5 <sklee@hanyang.ac.kr>
2024-08-06 19:51:13 +0900

    add dbinfo at .env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,15 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+
 const {Pool} = pkg;
+dotenv.config();
 
 const pool = new Pool({
-  user: 'quant',
-  host: 'localhost',
-  database: 'quantdb', 
-  password: 'quant14',
-  port: 8080,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export async function query(text: string, params?: any[]) {
